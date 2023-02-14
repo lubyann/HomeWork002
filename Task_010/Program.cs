9d5ca046313f08912b614962e59e924b0d3497a4
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-14 14:12:02 +0300

    add task10

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+//Console.Clear();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+//int number = new Random().Next(100, 1000);
+
+int result = number % 100 / 10;
+Console.WriteLine($"{number} -> {result}");
