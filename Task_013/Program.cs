4119de8dabeea04b73ea50a09da474605bf5d7f6
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-14 15:33:06 +0300

    add task013

--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -15,7 +15,23 @@ if (number < 100)
 {
     Console.WriteLine($"{number} -> третьей цифры нет");
 }
-else 
+// else
+// {
+//     Console.WriteLine($"{number} -> {number.ToString()[2]}");
+// }
+
+if (number >= 100 && number < 1000)
 {
-    Console.WriteLine($"{number} -> {number.ToString()[2]}");
+    int res = number % 10;
+    Console.WriteLine($"{number} -> {res}");
+}
+if (number < 10000 && number >= 1000)
+{
+    int res = number / 10 % 10;
+    Console.WriteLine($"{number} -> {res}");
+}
+if (number < 100000 && number >= 10000)
+{
+    int res = number / 100 % 10;
+    Console.WriteLine($"{number} -> {res}");
 }
